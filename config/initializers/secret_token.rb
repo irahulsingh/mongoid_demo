6dfaf5b7758502c9291cd47e7e71826b02cdef06
Rahul Singh <rahul692000@gmail.com>
2014-08-17 12:17:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongodbDemo::Application.config.secret_key_base = '59b440125866a352f106c184c180180682eb61b68a4b6c9e5ab2dff5e96612b880de58a632370c7c4317ed3c884a19e5bdde9a50b3cf32dccc018c06698d7db0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MongodbDemo::Application.config.session_store :cookie_store, key: '_mongodb_demo_session'
